6cf09bc9196d1f19ca72f92f9af146a77d9417a7
Brendan Geranio <abgeranio@gmail.com>
2021-12-23 14:58:09 -0800

    moved the message

--- index.js
+++ index.js
@@ -53,5 +53,5 @@ http.createServer(function (req, res) {
             res.end(content, 'utf-8');
         }
     });
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
